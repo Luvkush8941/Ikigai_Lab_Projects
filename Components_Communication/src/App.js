49811fab0b92c07f852291e2690de7a06bd1d914
Luvkush sharma <105363463+Luvkush8941@users.noreply.github.com>
2023-12-06 21:11:08 +0530

    Add files via upload

---
+++ Components_Communication/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Nature from "./Components/Nature";
+import Navbar from "./Components/Navbar";
+
+export default function App () {
+
+    return <>
+      <Navbar/>
+      <Header/>
+      <Nature/>
+      <Footer/>
+    </>
+}
